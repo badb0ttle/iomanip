f605cc2a6bc4c0d67434384de77cc24f141b1246
badb0ttle <2546744220@qq.com>
2025-10-25 16:32:00 +0800

    数论add

--- Manacher.cpp
+++ Manacher.cpp
@@ -1,19 +1,16 @@
-/*blank*/
+/*ordinary.exe*/
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long // 一时偷懒不收敛，乱开ll终遭谴
-#define ull unsigned long long 
-#define sc scanf
-#define pr printf
-#define PII pair<int, int>
-#define debug(x) cout << #x << '=' << x << '\n'
-const int N = 2e7 + 9;
+using i64=long long;
+const int N=2e7+9;
+string str;
 char s[N];
-int n,p[N];//求一个字符串最长回文子串长度or查询
+int n,p[N];
 void work()
 {
-    cin >> n;
-    cin>>s+1;
+    cin>>str;
+    n=(int)str.size();
+    for(int i=1;i<=n;++i)s[i]=str[i-1];
     for(int i=2*n+1;i>=1;--i)
     {
         if(i&1)s[i]='#';
@@ -25,27 +22,20 @@ void work()
     {
         p[i]=i<R?min(R-i,p[2*C-i]):1;
         while(s[i+p[i]]==s[i-p[i]])p[i]++;
-        if(p[i]+i>R)
-        {
+        if(p[i]+i>R){
             C=i;
             R=i+p[i];
         }
     }
     for(int i=1;i<=2*n+1;++i)
-    {
         ans=max(ans,p[i]-1);
-    }
     cout<<ans<<'\n';
 }
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int _ = 1;
-    // cin >> _;
-    // sc("%d",&_);
-    while (_--)
-        work();
-    return 0;
+int main() {
+ios::sync_with_stdio(0);
+cin.tie(0);
+int t=1;
+// cin >> t;
+while(t--)
+work();
 }
